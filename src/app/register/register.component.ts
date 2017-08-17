e0ec1b32ffadab87e6d6e5d81b10bbdf789d9bc8
Georgios Anastasiou <dr.george995@gmail.com>
2017-08-18 01:00:03 +0300

    made registration more robust

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit }       from '@angular/core';
 import { AuthenticationService }   from '../services/authentication.service';
+import { Router }                  from '@angular/router';
 
 @Component({
   templateUrl: './register.component.html',
@@ -7,30 +8,38 @@ import { AuthenticationService }   from '../services/authentication.service';
 
 export class RegisterComponent implements OnInit {
 
-    model: any = {};
-    error: String = "";
-    
-    constructor(private authenticationService: AuthenticationService) {}
+    private model: any = {};
+    private error: String = "";
+    private success: boolean = false;
+
+    constructor(private authenticationService: AuthenticationService, private router: Router) {}
 
     ngOnInit() {
         this.authenticationService.logout();
     }
 
     register() {
-        console.log(this.model.email);
-        this.authenticationService.register(this.model.email, this.model.password)
-        .subscribe(
-            (success) => {
-                if (success) {
-                    console.log("Registered");
+        console.log(this.model);
+        if(this.model.password !== this.model.password2) {
+            this.error = "Passwords do not match";
+            this.model.password = "";
+            this.model.password2 = "";
+        }
+        else if(this.model.password === undefined || this.model.email === undefined) {
+            this.error = "All fields are required";
+        }
+        else {
+            this.authenticationService.register(this.model.email, this.model.password)
+            .subscribe(
+                (success) => {
+                    this.error = "";
+                    this.success = true;
+                },
+                (error) => {
+                    // TODO: add more cases here
+                    this.error = "A user with that email already exists";
                 }
-                else {
-                    this.error = ("Failed to register");
-                }
-            },
-            (error) => {
-                this.error = "Please provide all required information";
-            }
-        )
+            );
+        }
     }
 }
