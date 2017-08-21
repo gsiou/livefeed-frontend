6208740a6d89c475dc3f7047363421a52dad8f1f
Georgios Anastasiou <dr.george995@gmail.com>
2017-08-22 00:02:41 +0300

    added angular cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-    templateUrl: 'app/app.html',
+    templateUrl: './app.html',
 })
 export class AppComponent  { name = 'Angular'; }
