27782fe176609128319546ceb64272dd64bde4e8
Georgios Anastasiou <dr.george995@gmail.com>
2017-09-30 02:29:09 +0300

    Changed indentation on timeline component (many more need fixing)

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -11,207 +11,207 @@ import { ArticleComponent } from './article.component';
 import { MenuComponent } from './../menu/menu.component';
 
 @Component({
-    templateUrl: './timeline.component.html',
+  templateUrl: './timeline.component.html',
 })
 
 export class TimelineComponent implements OnInit {
 
-    showAddFeedForm: boolean = false;
-    showAddAlertForm: boolean = false;
-    showManage: boolean = false;
-    url: string = "";
-    alert: string = "";
-    message: string = "";
-    error: string = "";
-    feeds: Feed[] = [];
-    timeline: Article[] = [];
-    nextTimeline: Article[] = null;
-    alerts: Alert[] = [];
-    loading: boolean = false;
-    filter: string = "";
+  showAddFeedForm: boolean = false;
+  showAddAlertForm: boolean = false;
+  showManage: boolean = false;
+  url: string = "";
+  alert: string = "";
+  message: string = "";
+  error: string = "";
+  feeds: Feed[] = [];
+  timeline: Article[] = [];
+  nextTimeline: Article[] = null;
+  alerts: Alert[] = [];
+  loading: boolean = false;
+  filter: string = "";
 
-    constructor(private feedService: FeedService, private alertService: AlertService) {}
+  constructor(private feedService: FeedService, private alertService: AlertService) {}
 
-    ngOnInit() {
-        if(localStorage.getItem('storedTimeline')) {
-            this.timeline = this.parseTimeline(localStorage.getItem('storedTimeline'));
-            this.loading = true;
-            this.fetchFeeds((articles: Article[]) => {
-                this.nextTimeline = articles;
-                this.loading = false;
+  ngOnInit() {
+    if(localStorage.getItem('storedTimeline')) {
+      this.timeline = this.parseTimeline(localStorage.getItem('storedTimeline'));
+      this.loading = true;
+      this.fetchFeeds((articles: Article[]) => {
+        this.nextTimeline = articles;
+        this.loading = false;
+      });
+    }
+    else {
+      this.fetchFeeds((articles: Article[]) => {
+        this.timeline = articles;
+        localStorage.setItem('storedTimeline', JSON.stringify(articles));
+      });
+    }
+    setInterval(() => {
+      this.fetchFeeds((articles: Article[]) => { this.nextTimeline = articles });
+    }, 5 * 60 * 1000);
+
+    this.fetchAlerts();
+  }
+
+  parseTimeline(stringifiedTimeline: string): Article[] {
+    // TODO: way too hacky
+    let timeline: Article[] = JSON.parse(stringifiedTimeline);
+    for(var i = 0; i < timeline.length; i++) {
+      timeline[i].pubdate = new Date(timeline[i].pubdate);
+    }
+    return timeline;
+  }
+
+  refreshTimeline() {
+    this.timeline = this.nextTimeline;
+    localStorage.setItem('storedTimeline', JSON.stringify(this.timeline));
+    this.nextTimeline = null;
+  }
+
+  fetchAlerts() {
+    this.alertService.fetchAlerts().subscribe(
+      (alerts) => {
+        //
+      },
+      (error) => {
+        //
+      }
+    )
+  }
+
+  fetchFeeds(callback: any) {
+    this.feedService.loadFeeds().subscribe(
+      (feeds) => {
+        this.feeds = feeds;
+        this.feedService.fetchAll(this.feeds).subscribe(
+          (data) => {
+            let allArticles: Article[] = [];
+            for(var i = 0; i < data.length; i++) {
+              allArticles = allArticles.concat(data[i]);
+            }
+            allArticles.sort(function(a, b){
+              if(a.pubdate < b.pubdate) { return 1; }
+              else if(a.pubdate > b.pubdate) { return -1; }
+              else { return 0; }
             });
+            callback(allArticles);
+          },
+          (error) => {
+          }
+        );
+      },
+      (error) => {
+        this.error = error;
+        this.message = "";
+      }
+    );
+  }
+
+  addFeed() {
+    this.feedService.addFeed(this.url).subscribe(
+      (response) => {
+        if (response === true) {
+          this.error = "";
+          this.message = "Feed added!";
         }
         else {
-            this.fetchFeeds((articles: Article[]) => {
-                this.timeline = articles;
-                localStorage.setItem('storedTimeline', JSON.stringify(articles));
-            });
+          this.error = "Failed to add feed"
+          this.message = "";
         }
-        setInterval(() => {
-            this.fetchFeeds((articles: Article[]) => { this.nextTimeline = articles });
-        }, 5 * 60 * 1000);
+        this.fetchFeeds((articles: Article[]) => {this.nextTimeline = articles});
+      },
+      (error) => {
+        this.error = error;
+        this.message = "";
+      }
+    )
+  }
 
-        this.fetchAlerts();
-    }
-
-    parseTimeline(stringifiedTimeline: string): Article[] {
-        // TODO: way too hacky
-        let timeline: Article[] = JSON.parse(stringifiedTimeline);
-        for(var i = 0; i < timeline.length; i++) {
-            timeline[i].pubdate = new Date(timeline[i].pubdate);
+  addAlert() {
+    this.alertService.addAlert(this.alert).subscribe(
+      (response) => {
+        if(response === true){
+          this.error = "";
+          this.message = "Alert added!";
         }
-        return timeline;
-    }
-
-    refreshTimeline() {
-        this.timeline = this.nextTimeline;
-        localStorage.setItem('storedTimeline', JSON.stringify(this.timeline));
-        this.nextTimeline = null;
-    }
-
-    fetchAlerts() {
-        this.alertService.fetchAlerts().subscribe(
-            (alerts) => {
-                //
-            },
-            (error) => {
-                //
-            }
-        )
-    }
-
-    fetchFeeds(callback: any) {
-        this.feedService.loadFeeds().subscribe(
-            (feeds) => {
-                this.feeds = feeds;
-                this.feedService.fetchAll(this.feeds).subscribe(
-                    (data) => {
-                        let allArticles: Article[] = [];
-                        for(var i = 0; i < data.length; i++) {
-                            allArticles = allArticles.concat(data[i]);
-                        }
-                        allArticles.sort(function(a, b){
-                            if(a.pubdate < b.pubdate) { return 1; }
-                            else if(a.pubdate > b.pubdate) { return -1; }
-                            else { return 0; }
-                        });
-                        callback(allArticles);
-                    },
-                    (error) => {
-                    }
-                );
-            },
-            (error) => {
-                this.error = error;
-                this.message = "";
-            }
-        );
-    }
-
-    addFeed() {
-        this.feedService.addFeed(this.url).subscribe(
-            (response) => {
-                if (response === true) {
-                    this.error = "";
-                    this.message = "Feed added!";
-                }
-                else {
-                    this.error = "Failed to add feed"
-                    this.message = "";
-                }
-                this.fetchFeeds((articles: Article[]) => {this.nextTimeline = articles});
-            },
-            (error) => {
-                this.error = error;
-                this.message = "";
-            }
-        )
-    }
-
-    addAlert() {
-        this.alertService.addAlert(this.alert).subscribe(
-            (response) => {
-                if(response === true){
-                    this.error = "";
-                    this.message = "Alert added!";
-                }
-                else {
-                    this.error = "Failed to add alert";
-                    this.message = "";
-                }
-            },
-            (error) => {
-                this.error = error;
-                this.message = "";
-            }
-
-        )
-    }
-
-    differentTimelines() {
-        if(this.timeline.length != this.nextTimeline.length) {
-            return true;
+        else {
+          this.error = "Failed to add alert";
+          this.message = "";
         }
+      },
+      (error) => {
+        this.error = error;
+        this.message = "";
+      }
 
-        for(var i = 0; i < this.timeline.length; i++) {
-            if(this.timeline[i].url != this.nextTimeline[i].url) {
-                return true;
-            }
+    )
+  }
+
+  differentTimelines() {
+    if(this.timeline.length != this.nextTimeline.length) {
+      return true;
+    }
+
+    for(var i = 0; i < this.timeline.length; i++) {
+      if(this.timeline[i].url != this.nextTimeline[i].url) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  onShowNewFeedForm(show: boolean) {
+    this.showAddFeedForm = show;
+  }
+
+  hideNewFeedForm() {
+    this.showAddFeedForm = false;
+  }
+
+  onSearch(terms: string) {
+    this.filter = terms;
+  }
+
+  onShowManage() {
+    this.showManage = true;
+  }
+
+  onHideManage() {
+    this.showManage = false;
+  }
+
+  applyFilter(article: Article): boolean {
+    let searchFilter = this.filter.toLowerCase();
+    if(article.title.toLowerCase().indexOf(searchFilter) !== -1) {
+      return true;
+    }
+    if(article.summary.toLowerCase().indexOf(searchFilter) !== -1) {
+      return true;
+    }
+    return false;
+  }
+
+  clearSearch() {
+    this.filter = "";
+  }
+
+  removeFeed(feed: Feed) {
+    this.feedService.removeFeed(feed.url).subscribe(
+      (response) => {
+        if(response === true){
+          this.error = "";
+          this.fetchFeeds((articles: Article[]) => {this.nextTimeline = articles});
         }
-        return false;
-    }
-
-    onShowNewFeedForm(show: boolean) {
-        this.showAddFeedForm = show;
-    }
-
-    hideNewFeedForm() {
-        this.showAddFeedForm = false;
-    }
-
-    onSearch(terms: string) {
-        this.filter = terms;
-    }
-
-    onShowManage() {
-        this.showManage = true;
-    }
-
-    onHideManage() {
-        this.showManage = false;
-    }
-
-    applyFilter(article: Article): boolean {
-        let searchFilter = this.filter.toLowerCase();
-        if(article.title.toLowerCase().indexOf(searchFilter) !== -1) {
-            return true;
+        else {
+          this.error = "Failed to remove feed"
+          this.message = "";
         }
-        if(article.summary.toLowerCase().indexOf(searchFilter) !== -1) {
-            return true;
-        }
-        return false;
-    }
-
-    clearSearch() {
-        this.filter = "";
-    }
-
-    removeFeed(feed: Feed) {
-        this.feedService.removeFeed(feed.url).subscribe(
-            (response) => {
-                if(response === true){
-                    this.error = "";
-                    this.fetchFeeds((articles: Article[]) => {this.nextTimeline = articles});
-                }
-                else {
-                    this.error = "Failed to remove feed"
-                    this.message = "";
-                }
-            },
-            (error) => {
-                this.error = error;
-                // TODO: handle this
-            }
-        )
-    }
+      },
+      (error) => {
+        this.error = error;
+        // TODO: handle this
+      }
+    )
+  }
 }
